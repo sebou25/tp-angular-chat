3dad6f5c2393c865b2de95ddefc944ffcdcb99ed
kerkristof <kerkristof@gmail.com>
2018-07-17 23:00:05 +0200

    Modification message component

--- src/app/display-msg/display-msg.component.ts
+++ src/app/display-msg/display-msg.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InjectTabService} from '../inject-tab.service';
 
 @Component({
   selector: 'app-display-msg',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display-msg.component.css']
 })
 export class DisplayMsgComponent implements OnInit {
-
   constructor() { }
+  // messages;
+    messages = [
+      {auteur: "Alice", message:"Salut,comment ça va?", heure:"10h55"},
+      {auteur: "Michel", message:"Salut,c'est Michel", heure:"10h55"},
+      {auteur: "Noemie", message:"Hello, on va voir le match à quelle heure?", heure:"10h55"},
+    ];
 
   ngOnInit() {
+
+    // this.messages = InjectTabService.msgTab();
+
   }
 
 }
