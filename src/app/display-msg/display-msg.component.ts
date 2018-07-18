12d3a74bec4274fbcec6c4850cb8e7d3309716df
sebou25 <sebastien25girard08@gmail.com>
2018-07-18 15:15:06 +0200

    mise a jour avec heure et new css

--- src/app/display-msg/display-msg.component.ts
+++ src/app/display-msg/display-msg.component.ts
@@ -12,20 +12,21 @@ export class DisplayMsgComponent implements OnInit {
   //declaration des variables
  tableau;
  messages;
- heure; 
+ 
 
   ngOnInit() {
 //recupere le userTable qui est dans l'injecteur et le charge dans tableau et l'affiche dans l html
     this.tableau = this.myservice.userTab;
     this.messages = this.myservice.msgTab;
-    this.heure = this.myservice.donneHeure();
+    
   }
+
   //Recuperer les message saisi de l input
   messageSaisi = '';
   creatMessage(messageSaisi) {
     this.messageSaisi = messageSaisi;
   
 //injecter les saisie (creatUser) des input dans le tableau(userTab)
-   this.messages.push({auteur:"alice", message:this.messageSaisi, heure:this.heure});
+   this.messages.push({auteur:"alice", message:this.messageSaisi, heure:this.myservice.donneHeure()});
   }
 }
