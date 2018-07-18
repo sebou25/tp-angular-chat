d0edf0e3b817ebbda8302aeaeacc9d5afb1f6e8b
antoinelanco <antoinelanco@gmail.fr>
2018-07-18 19:25:58 +0200

    vider la saisi de text

--- src/app/display-msg/display-msg.component.ts
+++ src/app/display-msg/display-msg.component.ts
@@ -13,20 +13,21 @@ export class DisplayMsgComponent implements OnInit {
  tableau;
  messages;
  auteur = sessionStorage.getItem('pseudo');
- 
+
 
   ngOnInit() {
 //recupere le userTable qui est dans l'injecteur et le charge dans tableau et l'affiche dans l html
     this.tableau = this.myservice.userTab;
     this.messages = this.myservice.msgTab;
-    
+
   }
 
   //Recuperer les message saisi de l input
   messageSaisi = '';
   creatMessage(messageSaisi) {
-    this.messageSaisi = messageSaisi;
-  
+    this.messageSaisi = messageSaisi.value;
+    messageSaisi.value = "";
+
 //injecter les saisie (creatUser) des input dans le tableau(userTab)
    this.messages.push({auteur:this.auteur, message:this.messageSaisi, heure:this.myservice.donneHeure()});
   }
