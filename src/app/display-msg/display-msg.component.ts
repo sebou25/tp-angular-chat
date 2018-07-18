34c8ccb3f2529084fe3f1dca377c79090670cfd0
Kerkristof <kerkristof@gmail.Com>
2018-07-18 16:23:08 +0200

    maj

--- src/app/display-msg/display-msg.component.ts
+++ src/app/display-msg/display-msg.component.ts
@@ -9,16 +9,11 @@ import { Inject } from '@angular/compiler/src/core';
 })
 export class DisplayMsgComponent implements OnInit {
   constructor(private myservice: InjectTabService) { }
-<<<<<<< HEAD
-  tableau;
-
-=======
   //declaration des variables
  tableau;
  messages;
  auteur = sessionStorage.getItem('pseudo');
  
->>>>>>> 36858953d8417cad8ca285671acb02e9989589ba
 
   ngOnInit() {
 //recupere le userTable qui est dans l'injecteur et le charge dans tableau et l'affiche dans l html
