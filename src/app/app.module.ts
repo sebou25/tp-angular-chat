6d939dbe3fe47664235554cb885d0e7c6048bad2
sebou25 <sebastien25girard08@gmail.com>
2018-07-18 19:11:19 +0200

    essai maison

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreatCountComponent } from './creat-count/creat-count.component';
+import { DiscutionComponent } from './discution/discution.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatCountComponent,
+    DiscutionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([/*permet d'indiquer au module les changement de page*/
+      {
+        path: 'crea-count', /*permet de pointer sur le component newrott*/
+        component: NewrottComponent
+      }
   ],
   providers: [],
   bootstrap: [AppComponent]
